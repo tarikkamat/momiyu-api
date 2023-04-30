05c7b75faf2a896a5d23e5e3ae7659cacf845da0
Tarık KAMAT <tarikkamat2@gmail.com>
2023-04-30 17:07:29 +0300

    role model add

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,6 +6,12 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
+
+    protected $routeMiddleware = [
+        'role' => \App\Http\Middleware\EnsureUserHasRole::class,
+        // Other route middleware...
+    ];
+
     /**
      * The application's global HTTP middleware stack.
      *
@@ -39,7 +45,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
