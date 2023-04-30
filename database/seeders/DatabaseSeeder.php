6902a7cf091094fb2d172a2d7e0339eb4546ebba
Tarık KAMAT <tarikkamat2@gmail.com>
2023-04-30 17:27:14 +0300

    seeder created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+            'email_verified_at' => '2021-04-30 14:00:00',
+            'remember_token' => '1'
+        ]);
+
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'customer']);
+
+        DB::table('role_user')->insert([
+            'role_id' => 1,
+            'user_id' => 11
+        ]);
+
+
     }
 }
