5a2e6bf36ee892b1beb733078f424a1be190fbfe
Tarık KAMAT <tarikkamat2@gmail.com>
2023-04-30 16:05:31 +0300

    api check route add

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/check', function () {
+    return response()->json([
+        'message' => 'Running',
+    ], 200);
+});
